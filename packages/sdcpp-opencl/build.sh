25bc66049e661b874090af45a6e9d6365151e9b2
Edward Wheatbow <114430143+Wheatbow@users.noreply.github.com>
2025-08-04 13:27:49 +0200

    bump(main/sdcpp-opencl): 5b8996f

--- packages/sdcpp-opencl/build.sh
+++ packages/sdcpp-opencl/build.sh
@@ -2,16 +2,15 @@ TERMUX_PKG_HOMEPAGE=https://github.com/leejet/stable-diffusion.cpp.git
 TERMUX_PKG_DESCRIPTION="Stable Diffusion and Flux in pure C/C++"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-__GIT_REV="f6b9aa1"
+__GIT_REV="5b8996f"
 TERMUX_PKG_VERSION=0.0.0-${__GIT_REV}
 TERMUX_PKG_SRCURL=https://github.com/leejet/stable-diffusion.cpp/archive/refs/tags/master-${__GIT_REV}.tar.gz
 TERMUX_PKG_SHA256="SKIP_CHECKSUM"
-TERMUX_PKG_BUILD_DEPENDS="opencl-headers, vulkan-headers, ocl-icd, shaderc"
+TERMUX_PKG_BUILD_DEPENDS="opencl-headers"
 TERMUX_PKG_CONFLICTS="sdcpp"
 TERMUX_PKG_REPLACES="sdcpp"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DSD_OPENCL=ON \
--DSD_VULKAN=OFF \
 -DGGML_OPENMP=OFF
 "
 termux_step_post_get_source() {
