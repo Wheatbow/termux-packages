d8adf3826e7f76a869b78e74e409bfdf64ca9eba
Edward Wheatbow <114430143+Wheatbow@users.noreply.github.com>
2026-03-05 11:55:00 +0100

    bump(main/llamacpp-custom): b8204

--- packages/llamacpp-custom/build.sh
+++ packages/llamacpp-custom/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ggml-org/llama.cpp
 TERMUX_PKG_DESCRIPTION="LLM inference in C/C++"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-__VERSION="b8194"
+__VERSION="b8204"
 TERMUX_PKG_VERSION=0.0.0-${__VERSION}
 TERMUX_PKG_SRCURL=https://github.com/ggml-org/llama.cpp/archive/refs/tags/${TERMUX_PKG_VERSION#*-}.tar.gz
 TERMUX_PKG_SHA256="SKIP_CHECKSUM"
@@ -21,7 +21,7 @@ termux_step_make_install() {
   local _pkgdir
   _pkgdir="$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX"
   mkdir -p "$_pkgdir/bin" "$_pkgdir/lib"
-  cp "$TERMUX_PKG_BUILDDIR"/bin/libggml*.so "$_pkgdir/lib"
+  cp "$TERMUX_PKG_BUILDDIR"/bin/*.so "$_pkgdir/lib"
   cp "$TERMUX_PKG_BUILDDIR"/bin/llama* "$_pkgdir/bin"
 }
 
