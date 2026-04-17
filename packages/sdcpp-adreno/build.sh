fb08021fe9ff5f6a8f08f54c1edfdfe20ebb2368
Edward Wheatbow <114430143+Wheatbow@users.noreply.github.com>
2026-04-17 15:00:37 +0200

    bump(main/sdcpp-adreno): 572-1b4e9be

--- packages/sdcpp-adreno/build.sh
+++ packages/sdcpp-adreno/build.sh
@@ -2,16 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://github.com/leejet/stable-diffusion.cpp.git
 TERMUX_PKG_DESCRIPTION="Stable Diffusion and Flux in pure C/C++"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-__VERSION="545-1d6cb0f"
+__VERSION="572-1b4e9be"
 TERMUX_PKG_VERSION=0.0.0-${__VERSION}
 TERMUX_PKG_GIT_BRANCH="master"
 TERMUX_PKG_SRCURL=git+https://github.com/leejet/stable-diffusion.cpp
 TERMUX_PKG_SHA256="SKIP_CHECKSUM"
 TERMUX_PKG_BUILD_DEPENDS="opencl-headers"
+TERMUX_PKG_DEPENDS="libwebp"
 TERMUX_PKG_CONFLICTS="sdcpp, sdcpp-vulkan"
 TERMUX_PKG_REPLACES="sdcpp, sdcpp-vulkan"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DSD_OPENCL=ON \
+-DSD_USE_SYSTEM_WEBP=ON \
 -DGGML_OPENMP=OFF
 "
 
