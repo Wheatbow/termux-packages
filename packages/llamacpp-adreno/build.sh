a39cebdb7bb72aed1f85ff418e46b2c2bafc891a
Edward Wheatbow <114430143+Wheatbow@users.noreply.github.com>
2026-06-05 15:27:00 +0200

    bump(main/llamacpp-adreno): b9521

--- packages/llamacpp-adreno/build.sh
+++ packages/llamacpp-adreno/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/ggml-org/llama.cpp
 TERMUX_PKG_DESCRIPTION="LLM inference in C/C++"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-__VERSION="b9487"
+__VERSION="b9521"
 TERMUX_PKG_VERSION=0.0.0-${__VERSION}
 TERMUX_PKG_SRCURL=https://github.com/ggml-org/llama.cpp/archive/refs/tags/${TERMUX_PKG_VERSION#*-}.tar.gz
 TERMUX_PKG_SHA256="SKIP_CHECKSUM"
@@ -10,12 +10,19 @@ TERMUX_PKG_BUILD_DEPENDS="vulkan-headers, shaderc, opencl-headers"
 TERMUX_PKG_CONFLICTS="llama-cpp, llama-cpp-backend-vulkan, llama-cpp-backend-opencl"
 TERMUX_PKG_REPLACES="llama-cpp, llama-cpp-backend-vulkan, llama-cpp-backend-opencl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_BUILD_TYPE=Release \
 -DHEXAGON_SDK_ROOT=$XDG_DATA_HOME/hexagon-sdk/6.4.0.2 \
 -DLLAMA_BUILD_TESTS=OFF \
+-DGGML_NATIVE=OFF \
+-DGGML_CPU_ARM_ARCH=armv8.6-a+dotprod+fp16+i8mm \
+-DGGML_CPU_KLEIDIAI=ON \
 -DGGML_VULKAN=ON \
 -DGGML_OPENCL=ON \
+-DGGML_OPENCL_USE_ADRENO_KERNELS=ON \
+-DGGML_SCHED_NO_REALLOC=ON \
+-DGGML_LTO=ON \
 -DGGML_HEXAGON=OFF \
--DGGML_OPENMP=OFF"
+-DGGML_OPENMP=ON"
 
 termux_step_make_install() {
   local _pkgdir
